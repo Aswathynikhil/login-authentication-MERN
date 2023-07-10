214bdb6c0a23260c196c4add272fb6d276c58ce0
Aswathy N <aswathia.achu@gmail.com>
2023-07-10 11:45:51 +0530

    username page design completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,30 @@
-
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Username from './components/Username';
+import Reset from './components/Reset';
+import Register from './components/Register';
+import Recovery from './components/Recovery';
+import Profile from './components/Profile';
+import Password from './components/Password';
+import PageNotFound from './components/PageNotFound';
 
 function App() {
   return (
-    <div className="App">
-     <h1>login</h1> 
-    </div>
-  );
+    <>
+          <Router>
+              <Routes>
+                  <Route path="/" element={<Username />} />
+                  <Route path="/reset" element={<Reset />} />
+                  <Route path="/register" element={<Register />} />
+                  <Route path="/profile" element={<Profile />} />
+                  <Route path="/recovery" element={<Recovery />} />
+                  <Route path="/password" element={<Password />} />
+                  <Route path="/pagenotfound" element={<PageNotFound />} />
+              </Routes>
+              
+     </Router>
+    </>
+  )
 }
 
-export default App;
+export default App
